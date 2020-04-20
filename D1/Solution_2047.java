618c81033547d89b7d69b54907e1381c838293d5
Hyun you jin <hyunyj21@gmail.com>
2020-04-21 02:32:07 +0900

    2047. 신문 헤드라인

---
+++ D1/Solution_2047.java
@@ -0,0 +1,15 @@
+/*
+ * [D1] 2047. 신문 헤드라인
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution_2047 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String input = in.readLine();
+		
+		System.out.println(input.toUpperCase());
+	}
+}
