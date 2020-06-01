46052088dc4cbc62b31fc93a0615aec4d6e105f5
Hyun you jin <hyunyj21@gmail.com>
2020-06-01 22:18:19 +0900

    --ing--

---
+++ D3/Solution_5356.java
@@ -0,0 +1,17 @@
+/*
+ * [D3] 5356. 의석이의 세로로 말해요
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution_5356 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(in.readLine());
+		
+		for (int t = 1; t <= tc; t++) {
+			
+		}
+	}
+}
