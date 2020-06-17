8c7807eea47a296ec42f9eb42f3ffcf27d386798
Hyun you jin <hyunyj21@gmail.com>
2020-06-17 23:53:46 +0900

    --ing

---
+++ D3/Solution_3131.java
@@ -0,0 +1,15 @@
+package d3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution_3131 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(in.readLine());
+		
+		for (int t = 1; t <= tc; t++) {
+			//내일까지 팀플ㅠㅠㅠ
+		}
+	}
+}
