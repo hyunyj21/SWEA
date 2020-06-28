c2e39d0c78162cb9354f1ca33354a39c381e64e8
Hyun you jin <hyunyj21@gmail.com>
2020-06-28 22:18:21 +0900

    1229. [S/W 문제해결 기본] 8일차 - 암호문2

---
+++ D3/Solution_1229.java
@@ -0,0 +1,55 @@
+/*
+ * [D3] 1229. [S/W 문제해결 기본] 8일차 - 암호문2
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Solution_1229 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		for (int t = 1; t <= 10; t++) {
+			in.readLine();
+			StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+			LinkedList<String> num = new LinkedList<>();
+			
+			for (int i = 0; i < 10; i++)
+				num.offer(st.nextToken());
+			
+			in.readLine();
+			st = new StringTokenizer(in.readLine(), " ");
+			while(st.hasMoreTokens()) {
+				char code = st.nextToken().charAt(0);
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken()) + x;
+				
+				switch (code) {
+					case 'I':
+						for (int i = x; i < y; i++) {
+							if (i > 9) st.nextToken();
+							else num.add(i, st.nextToken());
+						}
+						break;
+	
+					case 'D':
+						for (int i = x; i < y; i++) {
+							if (i > 9) break;
+							else num.remove(x);
+						}
+						break;
+				}
+			}
+			
+			sb.append("#").append(t);
+			for (int i = 0; i < 10; i++)
+				sb.append(" ").append(num.get(i));
+			sb.append("\n");
+		}
+		
+		System.out.print(sb);
+	}
+}
