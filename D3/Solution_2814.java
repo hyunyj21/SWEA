3f328ea8561dba9bb99799ed360ae6766cac73db
Hyun you jin <hyunyj21@gmail.com>
2020-08-28 21:58:16 +0900

    --ing--

--- D3/Solution_2814.java
+++ D3/Solution_2814.java
@@ -4,66 +4,38 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution_2814 {
-	static int N;
-	static int[] parent, depth, cnt;
+	static List<List<Integer>> graph;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		int tc = Integer.parseInt(in.readLine());
 		
 		for (int t = 1; t <= tc; t++) {
-			sb.append("#").append(t).append(" ");
 			StringTokenizer st = new StringTokenizer(in.readLine(), " ");
-			N = Integer.parseInt(st.nextToken()) + 1;
+			int N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
+			graph = new ArrayList<>();
 			
-			if (M == 0 || M == 1) {
-				sb.append(M+1).append("\n");
-				continue;
+			for (int i = 0; i <= N; i++) {
+				graph.add(new ArrayList<Integer>());
 			}
-			
-			parent = new int[N];
-			depth = new int[N];
-			cnt = new int[N];
-			for (int i = 1; i < N; i++)
-				parent[i] = i;
 			
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(in.readLine(), " ");
-				union(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				
+				graph.get(x).add(y);
+				graph.get(y).add(x);
 			}
 			
-			Arrays.sort(cnt);
-			sb.append(cnt[N-1]+1).append("\n");
+			//dfs 작성하기
+			
 		}
-		
-		System.out.println(sb);
-	}
-	
-	public static int findSet(int v) {
-		if (parent[v] == v) return v;
-		
-		parent[v] = findSet(parent[v]);
-		return parent[v];
-	}
-	
-	public static void union(int u, int v) {
-		u = findSet(u);
-		v = findSet(v);
-		
-		if (depth[u] > depth[v]) {
-			parent[v] = u;
-			cnt[u] += cnt[v] + 1;
-		} else {
-			parent[u] = v;
-			cnt[v] += cnt[u] + 1;
-		}
-		
-		if (depth[u] == depth[v]) depth[v]++;
 	}
 }
