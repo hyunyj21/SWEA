322a969c3ce90a6bb1066ac5f5e1c1b761e886d4
Hyun you jin <hyunyj21@gmail.com>
2020-10-11 23:53:12 +0900

    --ing--

---
+++ D3/Solution_1491.java
@@ -0,0 +1,22 @@
+/*
+ * [D3] 1491. 원재의 벽 꾸미기
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_1491 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(in.readLine());
+		
+		for (int t = 1; t <= tc; t++) {
+			StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+			int N = Integer.parseInt(st.nextToken());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+		}
+	}
+}
