39d353b994f6bb40845ad0075e7890695f5ed546
Hyun you jin <hyunyj21@gmail.com>
2020-11-25 23:13:33 +0900

    --ing--

---
+++ D3/Solution_10993.java
@@ -0,0 +1,90 @@
+/*
+ * [D3] 10993. 군주제와 공화제
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_10993 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int tc = Integer.parseInt(in.readLine());
+		
+		for (int t = 1; t <= tc; t++) {
+			int N = Integer.parseInt(in.readLine());
+			int[][] city = new int[N][3];
+			
+			for (int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(in.readLine());
+				city[i][0] = Integer.parseInt(st.nextToken());
+				city[i][1] = Integer.parseInt(st.nextToken());
+				city[i][2] = Integer.parseInt(st.nextToken());
+			}
+			
+			double[][] influence = new double[N][N];
+			
+			for (int i = 0; i < N; i++) {
+				for (int j = i+1; j < N; j++) {
+					double len = Math.pow(city[i][0]-city[j][0], 2) + Math.pow(city[i][1]-city[j][1], 2);
+					
+					if (city[i][2]/len > city[j][2]) influence[i][j] = city[i][2]/len;
+					else if (city[j][2]/len > city[i][2]) influence[j][i] = city[j][2]/len;
+				}
+			}
+			
+			char[] result = new char[N];
+			
+			for (int i = 0; i < N; i++) {
+				int cityNum = 0, tmp = 0;
+				double max = 0;
+				
+				for (int j = 0; j < N; j++) {
+					if (influence[j][i] == 0) continue;
+					if (influence[j][i] == max) {
+						tmp = 1;
+						break;
+					}
+					
+					if (influence[j][i] > max) {
+						tmp = 2;
+						max = influence[j][i];
+						cityNum = j+1;
+					}
+				}
+				
+				if (tmp == 0) result[i] = 'K';
+				else if (tmp == 1) result[i] = 'D';
+				else result[i] = (char) (cityNum + '0');
+			}
+			
+			sb.append("#").append(t);
+			
+			for (int i = 0; i < N; i++) {
+				sb.append(" ");
+				
+				if (!Character.isDigit(result[i])) {
+					sb.append(result[i]);
+					continue;
+				}
+				
+				changeRoot(result, i);
+				sb.append(result[i]-'0');
+			}
+			
+			sb.append("\n");
+		}
+		
+		System.out.print(sb);
+	}
+	
+	public static char changeRoot(char[] result, int idx) {
+		if (!Character.isDigit(result[idx])) {
+			return (char) (idx+1 + '0');
+		}
+		
+		result[idx] = changeRoot(result, result[idx]-'0'-1);
+		return result[idx];
+	}
+}
