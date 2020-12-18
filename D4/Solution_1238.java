e7bdc39be6921682725a6e6f623fbeae08f2fc26
Hyun you jin <hyunyj21@gmail.com>
2020-12-18 16:46:24 +0900

    1238. [S/W 문제해결 기본] 10일차 - Contact

--- D4/Solution_1238.java
+++ D4/Solution_1238.java
@@ -5,6 +5,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
@@ -38,7 +39,6 @@ public class Solution_1238 {
 			
 			ans = 0;
 			visited = new boolean[101];
-			visited[start] = true;
 			
 			bfs(start);
 			System.out.println("#"+t+" "+ans);
@@ -47,19 +47,39 @@ public class Solution_1238 {
 	
 	public static void bfs(int start) {
 		LinkedList<int[]> q = new LinkedList<>();
-		ArrayList<Integer> tmp = graph.get(start);
 		
-		for (int i = 0, len = tmp.size(); i < len; i++) {
-			int num = tmp.get(i);
-			
-			if (visited[num]) continue;
-			
-			q.offer(new int[] {num, 1});
-			visited[num] = true;
+		q.offer(new int[] {start, 1});
+		visited[start] = true;
+		
+		ArrayList<ArrayList<Integer>> cntAll = new ArrayList<>();
+		for (int i = 0; i < 101; i++) {
+			cntAll.add(new ArrayList<>());
 		}
+
+		int max = 1;
 		
 		while (!q.isEmpty()) {
+			int[] cur = q.poll();
+			int n = cur[0];
+			int cnt = cur[1];
 			
+			if (cnt > max) max = cnt;
+			cntAll.get(cnt).add(n);
+			
+			ArrayList<Integer> tmp = graph.get(n);
+			if (tmp == null) continue;
+			
+			for (int i = 0, len = tmp.size(); i < len; i++) {
+				int num = tmp.get(i);
+				
+				if (visited[num]) continue;
+				
+				q.offer(new int[] {num, cnt+1});
+				visited[num] = true;
+			}
 		}
+		
+		cntAll.get(max).sort(Collections.reverseOrder());
+		ans = cntAll.get(max).get(0);
 	}
 }
